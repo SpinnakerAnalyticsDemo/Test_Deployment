32aa4206735b8b7c29f954bbce4a54960c8e5f69
SpinnakerAnalyticsDemo <108324941+SpinnakerAnalyticsDemo@users.noreply.github.com>
2022-06-30 15:54:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def predict():
     return render_template('index.html', prediction_text=f'Based on your input of {Xval}, Your Predicted Value is ${output}')
   
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080)
